514104d739ca461fac6eb7956dd8360e2a6eade5
Fani <kalamarafani@gmail.com>
2023-12-15 14:36:55 +0100

    Quizzes added

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+
+    public Canvas[] quizCanvases;  
+
+    void Start()
+    {
+        DeactivateAllCanvases();
+
+        int randomCanvasIndex = Random.Range(0, quizCanvases.Length);
+        ActivateCanvas(randomCanvasIndex);
+    }
+
+    void ActivateCanvas(int canvasIndex)
+    {
+        if (canvasIndex >= 0 && canvasIndex < quizCanvases.Length)
+        {
+            quizCanvases[canvasIndex].gameObject.SetActive(true);
+        }
+    }
+
+    void DeactivateAllCanvases()
+    {
+        foreach (Canvas canvas in quizCanvases)
+        {
+            canvas.gameObject.SetActive(false);
+        }
+    }
+
+}
