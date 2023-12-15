4075a091ec3fa0e5fa5e7b0acbc1a5c3f8b7dbfa
cenacchi2000 <filo.cenacchi@gmail.com>
2023-12-15 19:26:59 +0100

    selection

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -3,30 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class CharacterSelection : MonoBehaviour
 {
-	public GameObject[] characters;
-	public int selectedCharacter = 0;
+    public GameObject[] characters;
+    public GameObject canvas;
+    public int selectedCharacter = 0;
 
-	public void NextCharacter()
-	{
-		characters[selectedCharacter].SetActive(false);
-		selectedCharacter = (selectedCharacter + 1) % characters.Length;
-		characters[selectedCharacter].SetActive(true);
-	}
+    public void NextCharacter()
+    {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter = (selectedCharacter + 1) % characters.Length;
+        characters[selectedCharacter].SetActive(true);
+    }
 
-	public void PreviousCharacter()
-	{
-		characters[selectedCharacter].SetActive(false);
-		selectedCharacter--;
-		if (selectedCharacter < 0)
-		{
-			selectedCharacter += characters.Length;
-		}
-		characters[selectedCharacter].SetActive(true);
-	}
+    public void PreviousCharacter()
+    {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter--;
+        if (selectedCharacter < 0)
+        {
+            selectedCharacter += characters.Length;
+        }
+        characters[selectedCharacter].SetActive(true);
+    }
 
-	public void StartGame()
-	{
-		PlayerPrefs.SetInt("selectedCharacter", selectedCharacter);
-		//SceneManager.LoadScene(1, LoadSceneMode.Single);
-	}
+    public void StartGame()
+    {
+        PlayerPrefs.SetInt("selectedCharacter", selectedCharacter);
+        canvas.SetActive(false); // Hide the canvas
+        characters[selectedCharacter].SetActive(true); // Show the selected character
+        //SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
 }
