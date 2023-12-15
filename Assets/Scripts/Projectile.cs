827a01cbbef680924a9cc4c8dbd934106c9f6d0d
Francesco <dttfrancesco@gmail.com>
2023-12-15 14:09:42 +0100

    Shooting dynamic

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,16 +2,51 @@
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] private float speed = 4f;
+    private Vector3 shootingDirection;
+    private GameObject indexTipObject;
+
+    private void Start()
+    {
+        GameObject gameManager = GameObject.Find("GameManager");
+        if (gameManager != null)
+        {
+            HandTracking handTrackingScript = gameManager.GetComponent<HandTracking>();
+            if (handTrackingScript != null)
+            {
+                indexTipObject = handTrackingScript.indexObject;
+            }
+        }
+
+        if (indexTipObject != null)
+        {
+            // Calcola la direzione usando la posizione della punta dell'indice
+            shootingDirection = (indexTipObject.transform.position - transform.position).normalized;
+        }
+        else
+        {
+            Debug.LogError("Index tip object non trovato.");
+        }
+    }
+
+    private void Update()
+    {
+        if (indexTipObject != null)
+        {
+            transform.Translate(shootingDirection * speed * Time.deltaTime, Space.World);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("track")) 
+        if (collision.gameObject.CompareTag("track"))
         {
-            // stop the object
+            // Stop the object
             Rigidbody rb = GetComponent<Rigidbody>();
             if (rb != null)
             {
-                rb.velocity = Vector3.zero; 
-                rb.isKinematic = true; 
+                rb.velocity = Vector3.zero;
+                rb.isKinematic = true;
             }
         }
     }
